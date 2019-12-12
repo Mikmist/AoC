7f65656d60c881d86f8c9b5d5b68663f90763b8b
Zino Holwerda <z.holwerda@ho2o.nl>
2019-12-12 04:44:45 +0100

    Added gitignore for target folder and AoC day 1 program

---
+++ 1/rust/src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+use std::f32;
+
+fn get_value(i: f32) -> f32 {
+    let value: f32 = (i/3.).floor() - 2.;
+    if value < 0. {
+        return 0.;
+    } else {
+        return value;
+    }
+}
+
+fn main() {
+    let mut line;
+    let mut sum = 0.0_f32;
+    let mut trimmed = " ";
+    
+    while trimmed != "-1" {
+        line = String::new();
+        io::stdin().read_line(&mut line)
+            .expect("Failed to read line.");    
+         
+        trimmed = line.trim();
+
+        if trimmed == "-1" {
+            break;
+        }
+        
+        match trimmed.parse::<f32>() {
+            Ok(i) => sum += get_value(i),
+            Err(..) => println!("this was not an integer: {}", trimmed),
+        };
+    }
+
+    println!("sum: {}", sum);
+}
