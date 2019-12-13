643ed54dafb37895b84f0fea5ff067cff561e23b
Zino Holwerda <z.holwerda@ho2o.nl>
2019-12-13 04:05:18 +0100

    Added dependency and main code, starting to learn rust

--- 12/rust/src/main.rs
+++ 12/rust/src/main.rs
@@ -1,8 +1,50 @@
+use regex::Regex;
+
+
 mod aoc;
+
+struct Moon {
+    pos: Vec<i32>,
+    vel: Vec<i32>,
+}
+
+impl Moon {
+    pub fn new() -> Self {
+        Moon {
+            pos: Vec::new(),
+            vel: Vec::new(),
+        }
+    }
+
+    fn from_line(line: String) -> Result<Self, String> {
+        let mut moon = Self::new();
+        let re = Regex::new(r"^<x=(.+), y=(.+), z=(.+)>$").unwrap();
+        
+        let groups = if let Some(capture) = re.captures(&line) {
+            capture
+        } else {
+            return Err(format!("Invalid line: {}", line));
+        };
+
+        for i in 0..3 {
+            let string = groups.get(i+1).unwrap().as_str();
+            if let Ok(x) = string.parse() {
+                moon.pos.push(x);
+                moon.vel.push(0);
+            } else {
+                return Err(format!("Invalid number: {}", string));
+            }
+        }
+
+        Ok(moon)
+    }
+}
 
 fn main() {
     let lines = aoc::get_input_lines("input".to_string());
+    let mut moons: Vec<Moon> = Vec::new();
+
     for line in lines {
-        println!("{:?}", line);
-    }
+        moons.push(Moon::from_line(line).expect("Something went wrong!"));
+    }    
 }
