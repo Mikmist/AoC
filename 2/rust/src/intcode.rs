206f2c85267246a97b5ded9d852cf0504687c70d
Zino <z.holwerda@ho2o.nl>
2019-12-14 01:09:07 +0100

    Added quite a few files

--- 2/rust/src/intcode.rs
+++ 2/rust/src/intcode.rs
@@ -1,33 +1,34 @@
-#[derive(Clone)]
+
+#[derive(Clone, PartialEq)]
 enum Operation {
+    End,
     Mult(i32, i32, i32),
     Add(i32, i32, i32),
 }
 
-
 #[derive(Clone)]
 struct Process {
     operation: Operation,
-    step_size: i32,
+    step_size: usize,
 }
 
 #[derive(Clone)]
 pub struct Computer {
-    intcode: Vec<i32>,
+    pub intcode: Vec<i32>,
     input: Vec<i32>,
     output: Vec<i32>,
-    index: i32,
+    index: usize,
 }
 
 impl Computer {
-    pub fn with_input_and_index(intcode: String, input: Vec<i32>, index: i32) -> Computer {
+    pub fn with_input_and_index(intcode: String, input: Vec<i32>, index: usize) -> Computer {
         Computer {
             intcode: intcode.split(",")
                 .map(|s| s.parse().unwrap())
                 .collect(),
             input: input,
             output: Vec::new(),
-            index: 0,
+            index: index,
         }
     }
 
@@ -37,43 +38,65 @@ impl Computer {
 
     pub fn run(&mut self) -> Result<Vec<i32>, &'static str> {
         loop {
-            let process = self.parse_instruction(1)?;
-            
-            break;
+            let process = self.parse_instruction()?;
+            println!("index: {:?}", self.index);
+
+            if process.operation == Operation::End {
+                println!("Finished");
+                break;
+            }
+            self.index += process.step_size;
+            println!("index: {:?}", self.index);
         }
 
         Ok(self.output.to_vec())
     }
 
-    fn parse_instruction(&mut self, instruction: i32) -> Result<Process, &'static str> {
-        match instruction {
+    fn parse_instruction(&mut self) -> Result<Process, &'static str> {
+        match self.intcode[self.index] {
             1 => Ok(Process {
-                operation: Operation::Add(1,1,1),
+                operation: Operation::Add(
+                    self.intcode[self.index+1],
+                    self.intcode[self.index+2],
+                    self.intcode[self.index+3]
+                ),
                 step_size: 4,
             }),
             2 => Ok(Process {
-                operation: Operation::Mult(1,1,1),
+                operation: Operation::Mult(
+                    self.intcode[self.index+1],
+                    self.intcode[self.index+2],
+                    self.intcode[self.index+3]
+                ),
                 step_size: 4,
+            }),
+            99 => Ok(Process {
+                operation: Operation::End,
+                step_size: 0,
             }),
             _ => Err("Not a know instruction"),
         }
+    }
+
+    pub fn get_intcode_value(&mut self, index: usize) {
+        self.intcode[index];
     }
 }
 
 #[test]
 fn test_from_intcode() {
-    let computer = Computer::from_intcode(String::from("1,0,0,0,99"), Vec::new());
+    let mut computer = Computer::with_input(String::from("1,0,0,0,99"), Vec::new());
     assert_eq!(computer.intcode, vec![1,0,0,0,99]);
 }
 
 #[test]
 fn test_run() {
-    let computer = Computer::from_intcode(String::from("1,0,0,0,99"), Vec::new());
+    let mut computer = Computer::with_input(String::from("1,0,0,0,99"), Vec::new());
     computer.run();
 }
 
 #[test]
 fn test_instruction_1() {
-    let computer = Computer::from_intcode(String::from("1,0,0,0,99"), Vec::new());
+    let mut computer = Computer::with_input(String::from("1,0,0,0,99"), Vec::new());
     computer.run();
 }
--- 2/rust/src/main.rs
+++ 2/rust/src/main.rs
@@ -1,5 +1,12 @@
 mod intcode;
+mod aoc;
 
 fn main() {
-    println!("Hello, world!");
+    let lines = aoc::get_input_lines(String::from("input"));
+
+    for line in lines {
+        let mut computer = intcode::Computer::with_input(line, Vec::new());
+        computer.run().expect("Some error occured.");
+        println!("Answer: {:?}", computer.intcode[0]);
+    }
 }
