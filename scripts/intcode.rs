238170c823d155425112c5c40825b47e8025545e
Zino <z.holwerda@ho2o.nl>
2019-12-25 14:40:01 +0100

    Added the base intcode

--- scripts/intcode.rs
+++ scripts/intcode.rs
@@ -1,17 +1,20 @@
-#[derive(Clone, PartialEq)]
+use std::collections::HashMap;
+
+#[derive(Clone, PartialEq, Debug)]
 enum Operation {
     End,
-    Mult(i64, i64, i64),
-    Add(i64, i64, i64),
-    Input(i64),
+    Mult(i64, i64, usize),
+    Add(i64, i64, usize),
+    Input(usize),
     Output(i64),
-    Jit(i64, usize),
-    Jif(i64, usize),
+    Jit(i64, i64),
+    Jif(i64, i64),
     LessThan(i64, i64, usize),
     Equals(i64, i64, usize),
+    AdjustRelative(i64)
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Process {
     operation: Operation,
     step_size: usize,
@@ -19,21 +22,28 @@ struct Process {
 
 #[derive(Clone)]
 pub struct Computer {
-    pub intcode: Vec<i64>,
+    pub intcode: HashMap<usize,i64>,
     input: Vec<i64>,
     output: Vec<i64>,
     index: usize,
+    relative_base: usize,
 }
 
 impl Computer {
-    pub fn with_input_and_index(intcode: String, input: Vec<i64>, index: usize) -> Computer {
+    pub fn with_input_and_index(intcode_string: String, input: Vec<i64>, index: usize) -> Computer {
+        let intcode_vec: Vec<i64> = intcode_string.split(",").map(|s| s.parse().unwrap()).collect();
+        let mut intcode: HashMap<usize,i64> = HashMap::new();
+
+        for i in 0..intcode_vec.len() {
+            intcode.insert(i, intcode_vec[i]);
+        }
+
         Computer {
-            intcode: intcode.split(",")
-                .map(|s| s.parse().unwrap())
-                .collect(),
+            intcode: intcode,
             input: input,
             output: Vec::new(),
             index: index,
+            relative_base: 0,
         }
     }
 
@@ -46,7 +56,6 @@ impl Computer {
 
         loop {
             let process = self.parse_process()?;
-
             if process.operation == Operation::End {
                 break;
             }
@@ -60,19 +69,32 @@ impl Computer {
         Ok(self.output.to_vec())
     }
 
+    fn insert_value(&mut self, index: usize, value: i64) {
+        match self.intcode.insert(index, value) {
+            Some(_value) => (),
+            None => (),
+        }
+    }
+
+    fn get_value(&self, index: usize) -> i64 {
+        if let Some(value) = self.intcode.get(&index) {
+            return *value;
+        } else {
+            return 0;
+        }
+    }
+
     fn execute_instruction(&mut self, process: &Process) -> Option<usize> {
         let mut step_size: Option<usize> = None;
 
         match process.operation {
-            Operation::Mult(a, b, pos) =>
-                self.intcode[pos as usize] = a * b,
-            Operation::Add(a, b, pos) =>
-                self.intcode[pos as usize] = a + b,
+            Operation::Mult(a, b, pos) => self.insert_value(pos, a * b),
+            Operation::Add(a, b, pos) => self.insert_value(pos, a + b),
             Operation::Input(pos) =>  {
-                self.intcode[pos as usize] = self.input[0];
+                self.insert_value(pos, self.input[0]);
                 self.input.remove(0);
             },
-            Operation::Output(pos) => self.output.push(pos),
+            Operation::Output(pos) => self.output.push(pos as i64),
             Operation::Jit(a, b) => {
                 if a != 0 {
                     self.index = b as usize;
@@ -87,18 +109,19 @@ impl Computer {
             },
             Operation::LessThan(a, b, pos) => {
                 if a < b {
-                    self.intcode[pos] = 1;
+                    self.insert_value(pos, 1);
                 } else {
-                    self.intcode[pos] = 0;
+                    self.insert_value(pos, 0);
                 }
             },
             Operation::Equals(a, b, pos) => {
                 if a == b {
-                    self.intcode[pos] = 1;
+                    self.insert_value(pos, 1);
                 } else {
-                    self.intcode[pos] = 0;
+                    self.insert_value(pos, 0);
                 }
             },
+            Operation::AdjustRelative(a) => self.relative_base = (self.relative_base as i64 + a) as usize,
             Operation::End => (),
         }
 
@@ -106,14 +129,14 @@ impl Computer {
     }
 
     fn parse_process(&mut self) -> Result<Process, &'static str> {
-        let instruction = self.intcode[self.index] % 100;
+        let instruction = self.intcode[&self.index] % 100;
 
         match instruction {
             1 => Ok(Process {
                 operation: Operation::Add(
                     self.parse_arg_value_from_mode(1),
                     self.parse_arg_value_from_mode(2),
-                    self.intcode[self.index+3],
+                    self.parse_pos_from_mode(3),
                 ),
                 step_size: 4,
             }),
@@ -121,13 +144,13 @@ impl Computer {
                 operation: Operation::Mult(
                     self.parse_arg_value_from_mode(1),
                     self.parse_arg_value_from_mode(2),
-                    self.intcode[self.index+3],
+                    self.parse_pos_from_mode(3),
                 ),
                 step_size: 4,
             }),
             3 => Ok(Process {
                 operation: Operation::Input(
-                    self.intcode[self.index+1]
+                    self.parse_pos_from_mode(1)
                 ),
                 step_size: 2,
             }),
@@ -140,14 +163,14 @@ impl Computer {
             5 => Ok(Process {
                 operation: Operation::Jit(
                     self.parse_arg_value_from_mode(1),
-                    self.parse_arg_value_from_mode(2) as usize
+                    self.parse_arg_value_from_mode(2)
                 ),
                 step_size: 3,
             }),
             6 => Ok(Process {
                 operation: Operation::Jif(
                     self.parse_arg_value_from_mode(1),
-                    self.parse_arg_value_from_mode(2) as usize
+                    self.parse_arg_value_from_mode(2)
                 ),
                 step_size: 3,
             }),
@@ -155,7 +178,7 @@ impl Computer {
                 operation: Operation::LessThan(
                     self.parse_arg_value_from_mode(1),
                     self.parse_arg_value_from_mode(2),
-                    self.intcode[self.index+3] as usize
+                    self.parse_pos_from_mode(3)
                 ),
                 step_size: 4,
             }),
@@ -163,9 +186,15 @@ impl Computer {
                 operation: Operation::Equals(
                     self.parse_arg_value_from_mode(1),
                     self.parse_arg_value_from_mode(2),
-                    self.intcode[self.index+3] as usize
+                    self.parse_pos_from_mode(3)
                 ),
                 step_size: 4,
+            }),
+            9 => Ok(Process {
+                operation: Operation::AdjustRelative(
+                    self.parse_arg_value_from_mode(1),
+                ),
+                step_size: 2,
             }),
             99 => Ok(Process {
                 operation: Operation::End,
@@ -176,26 +205,60 @@ impl Computer {
     }
 
     fn parse_arg_value_from_mode(&self, arg_number: usize) -> i64 {
-        let mut instruction = self.intcode[self.index] / 100;
+        let mut modes = self.intcode[&self.index] / 100;
         let mut mode = 0;
 
         for _i in 0..arg_number {
-            mode = instruction % 10;
-            instruction /= 10;
+            mode = modes % 10;
+            modes /= 10;
         }
 
         if mode == 1 {
-            return self.intcode[self.index+arg_number];
+            return self.get_value(self.index+arg_number);
+        } else if mode == 2 {
+            return self.get_value(
+                (
+                    self.relative_base as i64 +
+                    self.get_value(self.index + arg_number)
+                ) as usize
+            );
         } else {
-            return self.intcode[self.intcode[self.index+arg_number] as usize];
+            return self.get_value(self.get_value(self.index+arg_number) as usize);
         }
     }
+
+    fn parse_pos_from_mode(&self, arg_number: usize) -> usize {
+        let mut modes = self.intcode[&self.index] / 100;
+        let mut mode = 0;
+
+        for _i in 0..arg_number {
+            mode = modes % 10;
+            modes /= 10;
+        }
+
+        if mode == 1 {
+            return self.index+arg_number;
+        } else if mode == 2 {
+            return (self.relative_base as i64 + self.get_value(self.index + arg_number)) as usize;
+        } else {
+            return self.get_value(self.index+arg_number) as usize;
+        }
+    }
+}
+
+fn check_hash_to_vec(hashmap: HashMap<usize,i64>, vec: Vec<i64>) -> bool {
+    for i in 0..vec.len() {
+        if hashmap[&i] != vec[i] {
+            return false;
+        }
+    }
+    return true;
 }
 
 #[test]
 fn test_from_intcode() {
     let computer = Computer::with_input(String::from("1,0,0,0,99"), Vec::new());
-    assert_eq!(computer.intcode, vec![1,0,0,0,99]);
+    assert!(crate::intcode::check_hash_to_vec(computer.intcode, vec![1,0,0,0,99]));
 }
 
 #[test]
@@ -208,21 +271,21 @@ fn test_run() {
 fn test_instruction_add() {
     let mut computer = Computer::with_input(String::from("1,0,0,0,99"), Vec::new());
     computer.run();
-    assert_eq!(computer.intcode, vec![2,0,0,0,99]);
+    assert!(crate::intcode::check_hash_to_vec(computer.intcode, vec![2,0,0,0,99]));
 }
 
 #[test]
 fn test_instruction_mult() {
     let mut computer = Computer::with_input(String::from("2,3,0,3,99"), Vec::new());
     computer.run();
-    assert_eq!(computer.intcode, vec![2,3,0,6,99]);
+    assert!(crate::intcode::check_hash_to_vec(computer.intcode, vec![2,3,0,6,99]));
 }
 
 #[test]
 fn test_instruction_case_1() {
     let mut computer = Computer::with_input(String::from("1,1,1,4,99,5,6,0,99"), Vec::new());
     computer.run();
-    assert_eq!(computer.intcode, vec![30,1,1,4,2,5,6,0,99]);
+    assert!(crate::intcode::check_hash_to_vec(computer.intcode, vec![30,1,1,4,2,5,6,0,99]));
 }
 
 #[test]
@@ -230,12 +293,12 @@ fn test_instruction_input() {
     let input = 3;
     let mut computer = Computer::with_input(String::from("1101,2,1,4,99,0,99"), vec![input]);
     computer.run();
-    assert_eq!(computer.intcode, vec![input,2,1,4,3,0,99]);
+    assert!(crate::intcode::check_hash_to_vec(computer.intcode, vec![input,2,1,4,3,0,99]));
 }
 
 #[test]
 fn test_parse_modes() {
-    let mut computer = Computer::with_input(String::from("01001,1,1,4,99,5,6,0,99"), Vec::new());
+    let computer = Computer::with_input(String::from("01001,1,1,4,99,5,6,0,99"), Vec::new());
     assert_eq!(computer.parse_arg_value_from_mode(1), 1);
     assert_eq!(computer.parse_arg_value_from_mode(2), 1);
 }
@@ -258,4 +321,7 @@ fn test_parse_in_out_immediate() {
     computer = Computer::with_input(String::from("3,3,1107,-1,8,3,4,3,99"), vec![7]);
     computer.run();
     assert_eq!(computer.output[0], 1);
+    computer = Computer::with_input(String::from("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"), Vec::new());
+    computer.run();
+    assert_eq!(computer.output, vec![109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]);
 }
