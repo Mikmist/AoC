3addaa0c454b52124baa6b6235f591250528fda3
Zino Holwerda <z.holwerda@ho2o.nl>
2020-12-01 22:04:47 +0100

    added some optimiations and script fix

--- 2020/1/python/main.py
+++ 2020/1/python/main.py
@@ -1,20 +1,30 @@
 data_list = []
 
-with open('data') as data:
-    for line in data:
+with open('../input/input') as data:
+    for line in data.read().split(' '):
         data_list.append(int(line))
 
 # Part 1
+found = False
 for i in data_list:
+    if found:
+        break
     for j in data_list:
         if i + j == 2020:
             print(i * j)
+            found = True
             break
 
 # Part 2
+found = False
 for i in data_list:
+    if found:
+        break
     for j in data_list:
+        if found:
+            break
         for k in data_list:
             if (i + j + k) == 2020:
                 print(i * j * k)
+                found = True
                 break
