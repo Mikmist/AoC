b480b28f10e2355bb5d16249d8529cf23129a3fa
Zino Holwerda <zino.holwerda@gyrobian.com>
2021-12-07 20:34:36 +0100

    added day 6

--- scripts/util.d
+++ scripts/util.d
@@ -1,6 +1,6 @@
 int submit(int day, int answer) {
    	import std.process : execute;
-        import std.conv;
+    import std.conv;
 	auto dmd = execute(["./submit", to!string(day), to!string(answer)]);
    	return dmd.status;
 }
