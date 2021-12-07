eee2e04a840cbe71173c2bdda08b65cf70c7a5fa
Zino Holwerda <zino.holwerda@gyrobian.com>
2021-12-07 21:45:00 +0100

    added day 7

--- 2021/07/d/source/app.d
+++ 2021/07/d/source/app.d
@@ -1,8 +1,30 @@
-import std.stdio;
+import std.stdio, std.conv, std.string;
 import util : submit;
+import std.algorithm.sorting;
+import std.algorithm.iteration;
+import std.math.algebraic;
+import std.math.rounding;
+
+auto calculateFuelA() {
+  auto values = (to!(int[])(File("./../input/input").readln().split(',')).sort());
+  auto fuel = 0; 
+  foreach (value; values)
+    fuel += abs(values[$/2] - value);
+  return fuel;
+}
+
+auto calculateFuelB() {
+  auto values = (to!(int[])(File("./../input/input").readln().split(',')));
+  auto mean = to!int(values.mean());
+  auto fuel = 0;
+  foreach (value; values) {
+    auto n = abs(mean - value);
+    fuel += n*(n+1)/2;
+  }
+  return fuel;
+}
 
 void main() {
-
-
-  // submit(day, answer)
+  writeln("Part A: ", calculateFuelA());
+  writeln("Part B: ", calculateFuelB());
 }
