986b916f283b2c639e3b62fd006a7f06fe456b13
Zino Holwerda <z.holwerda@ho2o.nl>
2021-12-12 18:20:31 +0100

    added partial day

---
+++ 2021/12/d/source/app.d
@@ -0,0 +1,119 @@
+import std.stdio, std.string, std.conv, std.ascii;
+import util;
+
+struct Map
+{
+  string[][string] map;
+
+  this(File file) {
+    foreach (line; file.byLine)
+    {
+      auto caves = line.split('-');
+      if (caves[0] in map) {
+        auto current =  caves[1];
+        bool isIn = false;
+        foreach(path; map[caves[0]]) {
+          if (path == current) isIn = true;
+        }
+        if (!isIn) map[to!string(caves[0])] ~= to!string(caves[1]);
+      } else {
+        map[to!string(caves[0])] ~= to!string(caves[1]);
+      }
+      if (caves[1] in map) {
+        auto current =  caves[1];
+        bool isIn = false;
+        foreach(path; map[caves[1]]) {
+          if (path == current) isIn = true;
+        }
+        if (!isIn) map[to!string(caves[1])] ~= to!string(caves[0]);
+      } else {
+        map[to!string(caves[1])] ~= to!string(caves[0]);
+      }
+    }
+  }
+}
+
+int explorePathsA(Map* map, string current, string[] visited) {
+  if (current == "end") {
+    return 1;
+  }
+
+  foreach (visitor; visited)
+  {
+    if (toUpper(current) != current && current == visitor) {
+      return 0;
+    } 
+  }
+  
+  auto total = 0;
+  visited ~= current;
+  foreach (possiblePath; map.map[current])
+  {
+    total += map.explorePathsA(possiblePath, visited);
+  }
+  return total;
+}
+int count = 0;
+int explorePathsB(Map* map, string current, int[string] visited, bool smallDoublePassed = false) {
+  if (current == "end") {
+    writeln(visited, " x");
+    count++;
+    return 1;
+  }
+
+  if (
+    ("start" in visited && current == "start") ||
+    (current in visited && visited[current] > 1 && smallDoublePassed)
+  ) {
+    return 0;
+  }
+  if (current in visited) {
+    visited[current] += 1;
+    if (visited[current] > 1 && toUpper(current) != current) smallDoublePassed = true;
+    if (visited[current] > 2 && toUpper(current) != current) return 0;
+  }
+  else visited[current] = 1;
+
+  auto total = 0;
+  // writeln(visited, map.map[current]);
+  foreach (possiblePath; map.map[current])
+  {
+    // writeln(possiblePath);
+    total += map.explorePathsB(possiblePath, visited.dup, smallDoublePassed);
+  }
+  return total;
+}
+
+
+/** 
+ * 
+ * Params:
+ *   file = Todays input files.
+ * Returns: The answer to the given file.
+ */
+int partA(File file) {
+  // Call Part A code here.
+  auto map = new Map(file);
+  return map.explorePathsA("start", []);
+}
+
+/** 
+ * 
+ * Params:
+ *   file = Todays input files.
+ * Returns: The answer to the given file.
+ */
+int partB(File file) {
+  // Call Part B code here.
+  auto map = new Map(file);
+  int[string] visitors;
+  return map.explorePathsB("start", visitors);
+}
+
+void main() {
+  // Use these variables to decide what the runner needs to run.
+	bool runReal = false;
+	bool runB = true;
+
+  runAnswers(&partA, &partB, runReal, runB);
+}
---
+++ 2021/12/d/source/util.d
@@ -0,0 +1,73 @@
+import std.typecons, std.stdio, std.conv;
+
+int submit(int part, int answer) {
+   	import std.process : execute;
+    import std.conv;
+	auto dmd = execute(["./submit", to!string(part), to!string(answer)]);
+   	return dmd.status;
+}
+
+void runAnswers(
+	int function(File) partA,
+	int function(File) partB,
+	bool runReal = false,
+	bool b = false
+) {
+	writeln("Part A");
+	auto file = File("../input/test");
+	writeln(" - Test: ", partA(file));
+	file = File("../input/input");
+	if (runReal) writeln(" - Real: ", partA(file));
+	
+	if (b) {
+		writeln("\nPart B");
+		file = File("../input/test");
+		writeln(" - Test: ", partB(file));
+		file = File("../input/input");
+		if (runReal) writeln(" - Real: ", partB(file));
+	}
+}
+
+struct IntMap2D
+{
+	int[][] map;
+	
+	/** 
+	* Returns the value at given coordinates, will return -1 if invalid coordinates.
+	*/
+	auto getValueAt(int x, int y) {
+		if (x < 0 ||  y < 0 || y > (map.length) - 1 || x > (map[y].length) - 1) {
+			return -1;
+		}
+		return map[y][x];
+	}
+
+	/** 
+	* Returns the value at given coordinates, will not check if the coord exists.
+	*/
+	void setValueAt(int x, int y, int val) {
+		map[y][x] = val;
+	}
+
+	this(File file) {
+		map = [];
+		foreach (line; file.byLine)
+		{
+			int[] currentRow = [];
+			foreach (value; line)
+			currentRow ~= to!int(to!string(value));
+			map ~= currentRow;
+		}
+	}
+
+	void print() {
+		for (int y = 0; y < map.length; y++) {
+			for(int x = 0; x < map[y].length; x++)
+			{
+			write(getValueAt(x, y), " ");
+			}
+			writeln();
+		}
+		writeln();
+	}
+}
