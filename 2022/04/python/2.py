5e86e78699a2847070da327e602e28aedd66d219
Zino Holwerda <zino.holwerda@gyrobian.com>
2022-12-04 19:46:59 +0100

    added day 4 fix

--- 2022/04/python/2.py
+++ 2022/04/python/2.py
@@ -1,11 +1,8 @@
-def compare_sets(set1, set2):
-    return set1[0] <= set2[1] and set1[1] >= set2[0]
-
 with open('../input/input') as data:
     total = 0
     for line in data:
         sets = [[int(j) for j in i.split('-')] for i in line.strip().split(',')]
-        if compare_sets(sets[0], sets[1]) or compare_sets(sets[1], sets[0]): 
+        if sets[0][0] <= sets[1][1] and sets[0][1] >= sets[1][0]: 
             total += 1
     print(total)
 
